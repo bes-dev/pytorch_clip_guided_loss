5c663a3e6e4f264dbe37482169bc75cbabd3e5ae
BeS <sergei.o.belousov@gmail.com>
2021-12-02 14:12:00 +0300

    update setup.py

--- setup.py
+++ setup.py
@@ -14,7 +14,10 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 from setuptools import setup, find_packages
-from pip.req import parse_requirements
+try: # for pip >= 10
+    from pip._internal.req import parse_requirements
+except ImportError: # for pip <= 9.0.3
+    from pip.req import parse_requirements
 
 readme = open('README.md').read()
 
