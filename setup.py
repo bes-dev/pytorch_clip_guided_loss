3b46596bcb1f28dfde59d853159e2aa1c9bfe1af
BeS <sergei.o.belousov@gmail.com>
2021-12-02 14:16:19 +0300

    update setup.py

--- setup.py
+++ setup.py
@@ -14,17 +14,12 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 from setuptools import setup, find_packages
-try: # for pip >= 10
-    from pip._internal.req import parse_requirements
-except ImportError: # for pip <= 9.0.3
-    from pip.req import parse_requirements
 
 readme = open('README.md').read()
 
 VERSION = '2021.12.02.0'
 
-install_reqs = parse_requirements("requirements.txt")
-requirements = [str(ir.req) for ir in install_reqs]
+requirements = [req.strip() for req in open("requirements.txt").readlines()]
 
 setup(
     # Metadata
